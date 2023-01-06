75e0b6cc1c8a54251b3a66d5075af7248514c532
Pietras17 <piotr.rysz@wp.pl>
2023-01-06 10:03:34 +0100

    add basic plugin structure show plugin in admin menu page add basic styles and script files

--- wp-content/plugins/slider/slider.php
+++ wp-content/plugins/slider/slider.php
@@ -18,3 +18,31 @@ if (!defined("SLIDER_PLUGIN_DIR_PATH"))
 
 if (!defined("SLIDER_PLUGIN_URL"))
     define("SLIDER_PLUGIN_URL", plugins_url() . "/slider");
+
+
+// Add plugins to menu in admin sections
+
+function slider_plugin_menu()
+{
+    add_menu_page("Slider Garagenonline", "Slider - Home", "manage_options", "slides-option", "slides_option", "dashicons-embed-photo", 11);
+    add_submenu_page("slides-option", "Ustawienia ogólne", "Ustawienia ogólne", "manage_options", "slides-option", "slides_option");
+}
+
+add_action("admin_menu", "slider_plugin_menu");
+
+function slides_option()
+{
+    include_once(SLIDER_PLUGIN_DIR_PATH . SLIDER_INCLUDES_FOLDER . "slider-dashboard.php");
+}
+
+// Add basic styles and scripts
+
+function add_styles_and_script()
+{
+    // styles
+    wp_enqueue_style("style", SLIDER_PLUGIN_URL . '/assets/css/style.css');
+    // scripts
+    wp_enqueue_script("script", SLIDER_PLUGIN_URL . '/assets/js/slider-script.js');
+}
+
+add_action("init", "add_styles_and_script");
