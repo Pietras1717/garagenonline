4f65151fa9c450ab98c8d23e7323b37436c8deb2
Pietras17 <piotr.rysz@wp.pl>
2023-01-07 11:49:57 +0100

    add basic reset in templates styles

--- wp-content/themes/garagenonline/functions.php
+++ wp-content/themes/garagenonline/functions.php
@@ -2,6 +2,9 @@
 // Dodanie własnych styli i skryptów do heada
 function wpdocs_scripts_method()
 {
-    wp_enqueue_script('jquery', get_stylesheet_directory_uri() . '/js/jquery.min.js', array());
+    // scripts
+    wp_enqueue_script('jquery', get_stylesheet_directory_uri() . '/js/jquery.min.js', true);
+    // styles
+    wp_enqueue_script('theme', get_stylesheet_directory_uri() . '/css/style.css', true);
 }
 add_action('wp_enqueue_scripts', 'wpdocs_scripts_method');
