5123b912e9e0f2697dcdc2cfea7513cc6bef96cd
Pietras17 <piotr.rysz@wp.pl>
2023-01-07 12:06:39 +0100

    add style to template

--- wp-content/themes/garagenonline/functions.php
+++ wp-content/themes/garagenonline/functions.php
@@ -5,6 +5,6 @@ function wpdocs_scripts_method()
     // scripts
     wp_enqueue_script('jquery', get_stylesheet_directory_uri() . '/js/jquery.min.js', true);
     // styles
-    wp_enqueue_script('theme', get_stylesheet_directory_uri() . '/css/style.css', true);
+    wp_enqueue_style('theme', get_stylesheet_directory_uri() . '/css/style.css', true);
 }
 add_action('wp_enqueue_scripts', 'wpdocs_scripts_method');
